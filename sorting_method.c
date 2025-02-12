a29ff5774c28ad0f660ab36fb7f8edd2fddaba8b
veeresh <veereshgujjula254@gmail.com>
2025-02-12 10:42:23 +0530

    Add files via upload

---
+++ sorting_method.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+int main()
+{
+int arr[50],i,j,len;
+srand(getpid());
+for(i=0;i<50;i++)
+{
+arr[i]=rand()%100;
+}
+for(i=0;i<50;i++)
+{
+printf("%d ",arr[i]);
+}
+printf("\n");
+for(i=0;i<50;i++)
+{
+for(j=0;j<50-i-1;j++)
+{
+ if(arr[j]>arr[j+1])
+ {
+    int temp=arr[j];
+    arr[j]=arr[j+1];
+    arr[j+1]=temp;
+ }
+}
+}
+for(i=0;i<50;i++)
+{
+printf("%d ",arr[i]);
+}
+}
+
+#include<stdio.h>
+int main()
+{
+int arr[50],i,j,len;
+srand(getpid());
+for(i=0;i<50;i++)
+{
+arr[i]=rand()%100;
+}
+for(i=0;i<50;i++)
+{
+printf("%d ",arr[i]);
+}
+printf("\n");
+for(i=0;i<50;i++)
+{
+for(j=0;j<50-i-1;j++)
+{
+ if(arr[j]>arr[j+1])
+ {
+    int temp=arr[j];
+    arr[j]=arr[j+1];
+    arr[j+1]=temp;
+ }
+}
+}
+int least_value=arr[0];
+int highest_value=arr[50-1];
+printf("%d %d ",least_value,highest_value); 
+}
